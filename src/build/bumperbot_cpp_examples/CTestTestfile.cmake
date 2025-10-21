614efd4071ab3d0889dd0f5982ba1927eb837de4
ali <Ah.Alnukhali@wakeb.tech>
2025-10-21 10:12:00 +0300

    Removed build and installa and log files from src

--- src/build/bumperbot_cpp_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ali/ali_ws/autonomous/src/bumperbot_cpp_examples
-# Build directory: /home/ali/ali_ws/autonomous/src/build/bumperbot_cpp_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/build/bumperbot_py_examples/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ali/ali_ws/autonomous/src/install/bumperbot_py_examples'
