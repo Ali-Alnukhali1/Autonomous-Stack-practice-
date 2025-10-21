c26cbe1955872487cb5409fdadc0c3ef18434ae3
ali <Ah.Alnukhali@wakeb.tech>
2025-10-21 10:05:56 +0300

    Cleaned tracked build, install, and vscode directories

--- build/bumperbot_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ali/ali_ws/autonomous/src/bumperbot_controller
-# Build directory: /home/ali/ali_ws/autonomous/build/bumperbot_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
